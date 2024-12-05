eb41e44d283b86f845308cdff11689fb564011df
beligost <georgebelhi@gmail.com>
2024-12-05 11:48:17 +0200

    slider fix
    slider fix

--- src/blocks/modules/promo/promo.js
+++ src/blocks/modules/promo/promo.js
@@ -1,6 +1,8 @@
-let promoSwiper = new Swiper(".promo__slider", {
-    navigation: {
-      nextEl: ".promo__swiperBtn--next",
-      prevEl: ".promo__swiperBtn--prev",
-    },
+document.addEventListener("DOMContentLoaded", () => {
+    let promoSwiper = new Swiper(".promo__slider", {
+        navigation: {
+            nextEl: ".promo__swiperBtn--next",
+            prevEl: ".promo__swiperBtn--prev",
+        },
+    });
 });
--- src/blocks/modules/reviews/reviews.js
+++ src/blocks/modules/reviews/reviews.js
@@ -1,6 +1,8 @@
-let reviewSwiper = new Swiper(".reviews__swiper", {
-    navigation: {
-      nextEl: ".reviews__button--next",
-      prevEl: ".reviews__button--prev",
-    },
+document.addEventListener("DOMContentLoaded", () => {
+    let reviewSwiper = new Swiper(".reviews__swiper", {
+        navigation: {
+            nextEl: ".reviews__button--next",
+            prevEl: ".reviews__button--prev",
+        },
+    });
 });
